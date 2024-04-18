a7c68bbf947adbd717f90a99cdd180f6597eae22
MagicJinn <jinn.bz@mail.com>
2024-04-18 02:02:25 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+
+<div align = center >
+
+# DougDougify
+
+<br>
+
+Is your browser experience boring? Are you having  
+trouble finding engaging click-baity videos?
+
+**Fear no more.**
+
+This extension adds the famous  
+youtuber **DougDoug** to every thumbnail.
+
+<br>
+
+[![Button Download Firefox]][Download Firefox]  
+[![Button Download Chrome]][Download Chrome]  
+[![Button Download Edge]][Download Edge]
+
+</div>
+
+<br>
+
+## Notes
+
+-   This extension should be compatible with any Firefox / Chromium based browser.
+
+-   This extension was inspired by **[Unnecessary Inventions][UI YouTube]** and the browser extension **[Youtube Mrbeastify][UI MrBeastify]**.
+
+-   This extension is unofficial and not affiliated with DougDoug or YouTube.
+
+
+<!----------------------------------------------------------------------------->
+
+[Button Download Firefox]: http://img.shields.io/badge/Firefox-FF7139?style=for-the-badge&logoColor=white&logo=Firefox
+
+[Button Download Chrome]: http://img.shields.io/badge/Chrome-4285F4?style=for-the-badge&logoColor=white&logo=GoogleChrome
+
+[Button Download Edge]: http://img.shields.io/badge/Edge-0078D7?style=for-the-badge&logoColor=white&logo=MicrosoftEdge
+
+
+[Download Firefox]: http://addons.mozilla.org/en-GB/firefox/addon/youtube-DougDougify/
+[Download Chrome]: http://chrome.google.com/webstore/detail/youtube-DougDougify/dbmaeobgdodeimjdjnkipbfhgeldnmeb
+[Download Edge]: http://microsoftedge.microsoft.com/addons/detail/youtube-DougDougify/jabaaojkmmljhmnheeihppepcmiadhll
+
+[UI MrBeastify]: http://github.com/MagicJinn/MrBeastify-Youtube
+[UI YouTube]: http://www.youtube.com/@UnnecessaryInventions
---
+++ build.bat
@@ -0,0 +1,64 @@
+@echo off
+setlocal
+echo  _   __      ____                 _   _  __              ____        _ _     _           
+echo ^|  \/  ^|_ __^| __ ^)  ___  __ _ ___^| ^|_^(_^)/ _^|_   _       ^| __ ^) _   _^(_^) ^| __^| ^| ___ _ __ 
+echo ^| ^|\/^| ^| '__^|  _ \ / _ \/ _` / __^| __^| ^| ^|_^| ^| ^| ^|      ^|  _ \^| ^| ^| ^| ^| ^|/ _` ^|/ _ \ '__^|
+echo ^| ^|  ^| ^| ^|  ^| ^|_^) ^|  __/ ^(_^| \__ \ ^|_^| ^|  _^| ^|_^| ^|      ^| ^|_^) ^| ^|_^| ^| ^| ^| ^(_^| ^|  __/ ^|   
+echo ^|_^|  ^|_^|_^|  ^|____/ \___^|\__,_^|___/\__^|_^|_^|  \__, ^|      ^|____/ \__,_^|_^|_^|\__,_^|\___^|_^|   
+echo                                             ^|___/                                        
+
+:: Define variables
+set ZIP_NAME_FIREFOX=Firefox.zip
+set ZIP_NAME_CHROMIUM=Chromium.zip
+set SOURCE_FOLDER=%CD%
+set TEMP_FOLDER=%SOURCE_FOLDER%\temp
+set SEVEN_ZIP_PATH="C:\Program Files\7-Zip\7z.exe"
+
+:: Check if 7-Zip is installed
+if not exist %SEVEN_ZIP_PATH% (
+    echo WARNING: 7-Zip is not installed. Please install 7-Zip to use this script.
+    echo I wish I could use Windows' built in Compress-Archive feature, but that is completely busted for some reason.
+    pause
+    exit /b
+)
+
+:: Ensure the temp folder is clean
+if exist "%TEMP_FOLDER%" rmdir /s /q "%TEMP_FOLDER%"
+mkdir "%TEMP_FOLDER%"
+
+:: Copy files and folders individually to avoid cyclic copy
+echo Copying files to the temp directory...
+xcopy "%SOURCE_FOLDER%\manifest.json" "%TEMP_FOLDER%" /Q
+xcopy "%SOURCE_FOLDER%\mrbeastify.js" "%TEMP_FOLDER%" /Q
+xcopy "%SOURCE_FOLDER%\images" "%TEMP_FOLDER%\images\" /E /Q
+xcopy "%SOURCE_FOLDER%\icons" "%TEMP_FOLDER%\icons\" /E /Q
+
+:: Create Firefox zip folder using 7-Zip
+echo Creating Firefox zip folder...
+%SEVEN_ZIP_PATH% a -tzip "%SOURCE_FOLDER%\%ZIP_NAME_FIREFOX%" "%TEMP_FOLDER%\*" -bso0 -bsp0
+echo Firefox zip folder created successfully.
+
+:: Ensure the temp folder is clean
+if exist "%TEMP_FOLDER%" rmdir /s /q "%TEMP_FOLDER%"
+mkdir "%TEMP_FOLDER%"
+
+:: Copy files and folders for Chromium
+xcopy "%SOURCE_FOLDER%\mrbeastify.js" "%TEMP_FOLDER%" /Q
+xcopy "%SOURCE_FOLDER%\manifest v3.json" "%TEMP_FOLDER%" /Q
+xcopy "%SOURCE_FOLDER%\images" "%TEMP_FOLDER%\images\" /E /Q
+xcopy "%SOURCE_FOLDER%\icons" "%TEMP_FOLDER%\icons\" /E /Q
+
+:: Rename manifest for Chromium
+echo Preparing files for Chromium zip...
+rename "%TEMP_FOLDER%\manifest v3.json" "manifest.json"
+
+:: Create Chromium zip folder using 7-Zip
+echo Creating Chromium zip folder...
+%SEVEN_ZIP_PATH% a -tzip "%SOURCE_FOLDER%\%ZIP_NAME_CHROMIUM%" "%TEMP_FOLDER%\*" -bso0 -bsp0
+echo Chromium zip folder created successfully.
+
+:: Cleanup
+if exist "%TEMP_FOLDER%" rmdir /s /q "%TEMP_FOLDER%"
+
+echo All operations completed successfully.
+pause
